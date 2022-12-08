dadd77ae42733a82637757e8b3fa9619190ea830
yasmeen elkhateeb <yasmeenelkhateeb87@gimail.com>
2022-12-08 03:35:31 +0200

    Create ToDo App

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/home_screen.dart';
+
+void main(){
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        HomeScreen.routeName : (context) => HomeScreen(),
+
+      },
+      initialRoute: HomeScreen.routeName,
+    );
+  }
+
+}
